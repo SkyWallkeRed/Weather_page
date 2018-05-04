ee60ccf615d50ea6c8d84c5260a9939be6db252e
דוד אוחנה <davidmail000@gmail.com>
2018-05-04 18:56:52 +0200

    working nice style

--- public/api.js
+++ public/api.js
@@ -11,8 +11,10 @@ class Api {
         }).then(function(data) {
             let newCity = new Data()
             newCity.addWeather(data, $serchData)
-                // $('#temp').html("Temperature in " + city + " is " + data.query.results.channel.item.condition.temp + "°C");
-        });
+        }).catch(function() {
+            alert('enter a vald city name')
+            $('body').css('background-image', 'url(https://images.unsplash.com/photo-1506631610770-2e3eeae401b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c5dc54d8183138e4763a3e4a19b5dd6c&auto=format&fit=crop&w=1502&q=80)');
+        })
     }
 }
 const api = new Api();
--- public/data.js
+++ public/data.js
@@ -3,10 +3,6 @@ import { Weather } from './modle.js'
 import { view } from './view.js'
 var STORAGE_ID = 'GetWeather';
 
-
-
-// var weatherResult ;
-
 class Data {
     constructor() {
         // this.weatherResult = this.getFromLocalStorage();
@@ -16,12 +12,23 @@ class Data {
         let currentTemp = data.query.results.channel.item.condition.temp
         let weatherDiscription = data.query.results.channel.item.condition.text
             //
+        if (weatherDiscription == 'Cloudy') {
+            $('body').css('background-image', 'url(https://images.unsplash.com/photo-1522060532265-30b8e7694c06?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=332380f5afb1e8767dadcc4c334fd6f8&auto=format&fit=crop&w=1296&q=80)');
+        } else if (weatherDiscription == 'Partly Cloudy') {
+            $('body').css('background-image', 'url(https://images.unsplash.com/photo-1519886047039-7c2219938794?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2e15f9cd98ecf8f6492af3aa4570c169&auto=format&fit=crop&w=1228&q=80)');
+        } else if (weatherDiscription == 'Sunny') {
+            $('body').css('background-image', 'url(https://images.unsplash.com/photo-1495171852069-0e02de3ec89a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a999fc4aa680f90582493d6c9d3f7351&auto=format&fit=crop&w=1630&q=80)');
+        } else if (weatherDiscription == 'Mostly Clear') {
+            $('body').css('background-image', 'url(https://images.unsplash.com/photo-1419833173245-f59e1b93f9ee?ixlib=rb-0.3.5&s=cc359049551174327fc371a839464444&auto=format&fit=crop&w=1500&q=80)');
+        }
+        //
+
+        //
         let weatherData = new Weather(cityName, currentTemp, weatherDiscription);
         // console.log(weatherData);
         weatherResult.push(weatherData);
         this.saveToLocalStorage();
         view.updateDisplay();
-
     }
     removePost($clickedPost, index) {
         console.log($clickedPost + 'from remove func')
